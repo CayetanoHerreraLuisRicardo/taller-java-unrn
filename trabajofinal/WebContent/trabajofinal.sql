cfda95891c6d2b19a9c6d95ce3eeeb5fe89fedab
Winderfind@gmail.com <Winderfind@gmail.com@33d2613c-2304-736f-4ed5-b9ea7df18124>
2013-04-26 12:52:27 +0000

    Edición de imágenes.

--- trabajofinal/WebContent/trabajofinal.sql
+++ trabajofinal/WebContent/trabajofinal.sql
@@ -123,7 +123,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 21 de febrero de 2013 (Pegi: +16)</li>', 299, 1, 'img/accion/crysis3.jpg'),
+<li>Lanzamiento: 21 de febrero de 2013 (Pegi: +16)</li>', 299, 1, 'img/crysis3.jpg'),
 ('Tomb Rider','<li>En esta nueva aventura encarnaremos a una joven e inexperta Lara, que deberá enfrentarse a los diferentes peligros y extrañas criaturas que habitan en una isla de la costa de Japón, dentro de un mundo abierto (sand-box) con tintes de survival-horror.</li>
 <li>Plataforma: PlayStation 3 - PC - X360</li>
 <li>Desarrollador: Crystal Dynamics</li>
@@ -134,7 +134,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 5 de marzo de 2013 (Pegi: +18)</li>', 199, 1, 'img/accion/tombraider.jpg'),
+<li>Lanzamiento: 5 de marzo de 2013 (Pegi: +18)</li>', 199, 1, 'img/tombraider.jpg'),
 ('Dead Space 3','<li>Tercera entrega dentro de la serie Dead Space que destaca por ofrecer cooperativo online, una campaña con nuevos necromorfos, más duración, un inédito planeta helado y opciones de creación de armas.</li>
 <li>Plataforma: PlayStation 3 - PC - X360</li>
 <li>Desarrollador: Visceral Games</li>
@@ -145,7 +145,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 8 de febrero de 2013 (Pegi: +18)</li>', 359, 1, 'img/accion/deadspace3.jpg'),
+<li>Lanzamiento: 8 de febrero de 2013 (Pegi: +18)</li>', 359, 1, 'img/deadspace3.jpg'),
 ('Assassin''s Creed 3','<li>Juego sin reseña</li>', 359, 1, NULL),
 ('Far Cry','<li>Tercera entrega de la saga que regresará al exótico y peligroso entorno africano.</li>
 <li>Plataforma: PlayStation 3 - PC - X360</li>
@@ -157,7 +157,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 29 de noviembre de 2012 (Pegi: +18)</li>', 189, 1, 'img/accion/farcry3.jpg'),
+<li>Lanzamiento: 29 de noviembre de 2012 (Pegi: +18)</li>', 189, 1, 'img/farcry3.jpg'),
 ('DmC','<li>Nuevo capítulo de la popular saga de acción y fantasía creada por Capcom, cuyo desarrollo ha sido encomendado al estudio Ninja Theory en colaboración con Capcom US. En el juego veremos a un Dante más rápido y demoledor que nunca, pero también mucho más joven e inexperto que, aun así, no pierde sus incipientes aires de chulería.</li>
 <li>Plataforma: PlayStation 3 - PC - X360</li>
 <li>Desarrollador: Ninja Theory</li>
@@ -168,7 +168,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 15 de enero de 2013 (Pegi: +16)</li>', 259, 1, 'img/accion/dmc.jpg'),
+<li>Lanzamiento: 15 de enero de 2013 (Pegi: +16)</li>', 259, 1, 'img/dmc.jpg'),
 ('Watch Dogs','<li>Watch Dogs está ambientado en un futuro cercano en el que el jugador tiene en su poder infinidad de gadgets y de artilugios tecnológicos con los que sembrar el caos a su alrededor para llevar a cabo diferentes tipos de misiones. Un planteamiento mitad aventura mitad shooter que ha levantado admiración durante su presentación en sociedad.</li>
 <li>Plataforma: PlayStation 3 - PC - X360 - PS4 - Wii U</li>
 <li>Desarrollador: UbiSoft</li>
@@ -178,7 +178,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 2013 (Por determinar) (Pegi: +18)</li>', 199, 1, 'img/accion/watchdogs.jpg'),
+<li>Lanzamiento: 2013 (Por determinar) (Pegi: +18)</li>', 199, 1, 'img/watchdogs.jpg'),
 ('The Cave','<li>Juego sin reseña</li>', 99, 2, NULL),
 ('Amnesia: A Machine for Pigs','<li>Juego sin reseña</li>', 300, 2, NULL),
 ('Lucius','<li>Juego sin reseña</li>', 259, 2, NULL),
@@ -194,7 +194,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 26 de octubre de 2010 (Pegi: +12)</li>', 0, 3, 'img/accion/sims3.jpg'),
+<li>Lanzamiento: 26 de octubre de 2010 (Pegi: +12)</li>', 0, 3, 'img/sims3.jpg'),
 ('Plants Vs. Zombies','<li>Una inmensa horda de divertidos zombis se dispone a invadir tu casa, y tu única línea de defensa es un arsenal de 49 plantas "atrapazombis". Este es el divertido plateamiento de este interesante juego mezcla de estrategia, acción y puzle.</li>
 <li>Plataforma: PlayStation 3 - PC - X360 - 3DS - DS - Vita - Web - iPhone - iPad - Android - Mac</li>
 <li>Desarrollador: PopCap</li>
@@ -205,7 +205,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 9 de febrero de 2011 (Pegi: +12)</li>', 0, 3, 'img/accion/plants_vs_zombies.jpg'),
+<li>Lanzamiento: 9 de febrero de 2011 (Pegi: +12)</li>', 0, 3, 'img/plants_vs_zombies.jpg'),
 ('Spore','<li>Will Wright creador del exitoso "Los Sims" es el responsable de "Spore", un innovador juego evolutivo en el que el jugador creará sus propias criaturas en un mundo que se desarrolla por toda la galaxia.</li>
 <li>Plataforma: PC - DS - iPhone - Mac</li>
 <li>Desarrollador: Maxis</li>
@@ -216,7 +216,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 5 de septiembre de 2008 (Pegi: +12)</li>', 59, 3, 'img/accion/spore.jpg'),
+<li>Lanzamiento: 5 de septiembre de 2008 (Pegi: +12)</li>', 59, 3, 'img/spore.jpg'),
 ('Braid','<li>Desarrollado por el diseñador experimental Jonathan Blow, ganador del "Independent Games Festival", Braid es un juego que mezcla plataformas 2D con elementos puzle dentro de un sistema de juego que permite manipular el tiempo de la acción.</li>
 <li>Plataforma: PlayStation 3 - PC - X360</li>
 <li>Desarrollador: Hothead Games</li>
@@ -227,7 +227,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 17 de diciembre de 2009 (Pegi: +7)</li>', 149, 3, 'img/accion/braid.jpg'),
+<li>Lanzamiento: 17 de diciembre de 2009 (Pegi: +7)</li>', 149, 3, 'img/braid.jpg'),
 ('World of Goo','<li>Es un original juego, tanto en concepto como en estilo gráfico, en el que es posible construir a base de una gotas llamadas "goo", todo tipo de impresionantes estructuras con las que resolver montones de ingeniosos puzles.</li>
 <li>Plataforma: PC - Wii - iPhone - iPad - Android - Mac</li>
 <li>Desarrollador: 2D Boy</li>
@@ -238,18 +238,61 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 19 de junio de 2009 (Pegi: +3)</li>', 149, 3, 'img/accion/world_of_goo.jpg'),
-('Need For Speed: Most Wanted','<li>Juego sin reseña</li>', 299, 4, NULL),
+<li>Lanzamiento: 19 de junio de 2009 (Pegi: +3)</li>', 149, 3, 'img/world_of_goo.jpg'),
+('Need For Speed: Most Wanted','<li>Juego sin reseña</li>', 299, 4, 'img/nfs_mw.jpg'),
 ('GRID 2','<li>Secuela del trepidante juego de carreras desarrollado por Codemasters, que cuenta entre otras novedades con pleno la última evolución del premiado motor gráfico EGO.</li>
 <li>Plataforma: PlayStation 3 - PC - X360</li>
 <li>Desarrollador: Codemasters</li>
 <li>Distribuidor: Codemasters</li>
-<li>Lanzamiento: 31 de mayo de 2013</li>', 399, 4, NULL),
-('Euro Truck','<li>Juego sin reseña</li>', 99, 4, NULL),
-('WRC 3','<li>Juego sin reseña</li>', 109, 4, NULL),
-('ToCA Race Driver 3','<li>Juego sin reseña</li>', 0, 4, NULL),
-('F1 2012','<li>Juego sin reseña</li>', 289, 4, NULL),
-('Trackmania 2: Canyon','<li>Juego sin reseña</li>', 79, 4, NULL),
+<li>Lanzamiento: 31 de mayo de 2013</li>', 399, 4, 'img/grid2.jpg'),
+('Euro Truck','<li>Segunda entrega del simulador de conducción de vehículos pesados, que nos invita a recorrer una amplia selección de carreteras secundarias europeas.</li>
+<li>Plataforma: PC</li>
+<li>Desarrollador: SCS Software</li>
+<li>Distribuidor: Kaipioni</li>
+<li>Lanzamiento: 5 de octubre de 2012 (Pegi: +3)</li>', 99, 4, 'img/eurotruck.jpg'),
+('WRC 3','<li>Nueva entrega del simulador de conducción WRC desarrollado por Milestone que presenta un nuevo motor gráfico con efectos de iluminación y de partículas avanzados. El juego pone a disposición del jugador todos los tramos del campeonato actual de rallies junto con los más de 50 equipos oficiales.</li>
+<li>Plataforma: PlayStation 3 - PC - X360 - Vita</li>
+<li>Desarrollador: Milestone</li>
+<li>Distribuidor: Namco Bandai</li>
+<li>Jugadores: 1-16 (Competitivo: 16)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 12 de octubre de 2012 (Pegi: +3)</li>', 109, 4, 'img/wrc3.jpg'),
+('ToCA Race Driver 3','<li>Es la nueva edición de la exitosa saga automovilística de Codemaster, la cual cubre una gran variedad de deportes de motor. En ToCA Race Driver 3 la competición se desarrolla en circuitos de todo el mundo con dos modos de juego: "Gira mundial" o "Carrera profesional".</li>
+<li>Plataforma: PC - PSP - PS2 - XBOX</li>
+<li>Desarrollador: Codemasters</li>
+<li>Distribuidor: Codemasters</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 24 de febrero de 2006 (Pegi: +12)</li>', 0, 4, 'img/tocarace3.jpg'),
+('F1 2012','<li>Videojuego con licencia oficial de la temporada 2012 del campeonato de Fórmula Uno que que incluye todos los equipos, pilotos y circuitos oficiales. Esta entrega incluye varias novedades y mejoras respecto a F1 2011, entre ellas la incorporación de la "Prueba para Jóvenes Pilotos".</li>
+<li>Plataforma: PlayStation 3 - PC - X360 - Mac</li>
+<li>Desarrollador: Codemasters</li>
+<li>Distribuidor: Codemasters</li>
+<li>Jugadores: 1-16 (Competitivo: 2-16 / Cooperativo: 2)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 21 de septiembre de 2012 (Pegi: +3)</li>', 289, 4, 'img/f12012.jpg'),
+('Trackmania 2: Canyon','<li>Secuela del exitoso arcade de conducción desarrollado por Nadeo. Carreras vertiginosas, un buen número de vehículos, circuitos, ambientaciones, competiciones y una intensa jugabilidad reforzadas con nuevos modos de juego.</li>
+<li>Plataforma: PC</li>
+<li>Desarrollador: Nadeo</li>
+<li>Distribuidor: UbiSoft</li>
+<li>Jugadores: 1-200 (Competitivo: 2-200)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 14 de septiembre de 2011 (Pegi: +3)</li>', 79, 4, 'img/trackmania2.jpg'),
 ('FIFA 13','<li>Juego sin reseña</li>', 389, 5, NULL),
 ('PES 2013','<li>Juego sin reseña</li>', 389, 5, NULL),
 ('NBA 2K13','<li>Juego sin reseña</li>', 299, 5, NULL),
