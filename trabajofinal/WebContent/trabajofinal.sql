5f6dad53da1b5594499cec2bb4b79380c7b189ff
Winderfind@gmail.com <Winderfind@gmail.com@33d2613c-2304-736f-4ed5-b9ea7df18124>
2013-04-26 04:03:34 +0000

    Edición de los Productos.

--- trabajofinal/WebContent/trabajofinal.sql
+++ trabajofinal/WebContent/trabajofinal.sql
@@ -100,7 +100,7 @@ DROP TABLE IF EXISTS `trabajofinal`.`producto` ;
 CREATE  TABLE IF NOT EXISTS `trabajofinal`.`producto` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
-  `descrip` VARCHAR(500) NOT NULL ,
+  `descrip` VARCHAR(1024) NOT NULL ,
   `precio` DOUBLE NOT NULL ,
   `img_url` VARCHAR(500) NULL ,
   `categoria_id` INT NOT NULL ,
@@ -113,25 +113,138 @@ CREATE  TABLE IF NOT EXISTS `trabajofinal`.`producto` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id`,`img_url`) VALUES
-('Crysis 3','<li>Juego sin reseña</li>', 299, 1, NULL),
-('Tomb Rider','<li>Juego sin reseña</li>', 199, 1, NULL),
-('Dead Space 3','<li>Juego sin reseña</li>', 359, 1, NULL),
+('Crysis 3','<li>Tercera entrega de la saga de acción desarrollada por CryTek: Crysis. El juego se ambienta de nuevo en la ciudad de Nueva York, ahora más abierta y con mayor variedad de natural de escenarios, en pleno 2047.</li>
+<li>Plataforma: PlayStation 3 - PC - X360</li>
+<li>Desarrollador: CryTek</li>
+<li>Distribuidor: EA</li>
+<li>Jugadores: 1-12 (Competitivo: 2-12)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 21 de febrero de 2013 (Pegi: +16)</li>', 299, 1, 'img/accion/crysis3.jpg'),
+('Tomb Rider','<li>En esta nueva aventura encarnaremos a una joven e inexperta Lara, que deberá enfrentarse a los diferentes peligros y extrañas criaturas que habitan en una isla de la costa de Japón, dentro de un mundo abierto (sand-box) con tintes de survival-horror.</li>
+<li>Plataforma: PlayStation 3 - PC - X360</li>
+<li>Desarrollador: Crystal Dynamics</li>
+<li>Distribuidor: Eidos Interactive</li>
+<li>Jugadores: 1-8 (Competitivo: 2-8)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 5 de marzo de 2013 (Pegi: +18)</li>', 199, 1, 'img/accion/tombraider.jpg'),
+('Dead Space 3','<li>Tercera entrega dentro de la serie Dead Space que destaca por ofrecer cooperativo online, una campaña con nuevos necromorfos, más duración, un inédito planeta helado y opciones de creación de armas.</li>
+<li>Plataforma: PlayStation 3 - PC - X360</li>
+<li>Desarrollador: Visceral Games</li>
+<li>Distribuidor: EA</li>
+<li>Jugadores: 1-2 (Cooperativo: 2)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 8 de febrero de 2013 (Pegi: +18)</li>', 359, 1, 'img/accion/deadspace3.jpg'),
 ('Assassin''s Creed 3','<li>Juego sin reseña</li>', 359, 1, NULL),
-('Far Cry','<li>Juego sin reseña</li>', 189, 1, NULL),
-('DmC','<li>Juego sin reseña</li>', 259, 1, NULL),
-('Wath Dogs','<li>Juego sin reseña</li>', 199, 1, NULL),
+('Far Cry','<li>Tercera entrega de la saga que regresará al exótico y peligroso entorno africano.</li>
+<li>Plataforma: PlayStation 3 - PC - X360</li>
+<li>Desarrollador: UbiSoft</li>
+<li>Distribuidor: UbiSoft</li>
+<li>Jugadores: 1-16 (Competitivo: 2-16 / Cooperativo: 2-4)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 29 de noviembre de 2012 (Pegi: +18)</li>', 189, 1, 'img/accion/farcry3.jpg'),
+('DmC','<li>Nuevo capítulo de la popular saga de acción y fantasía creada por Capcom, cuyo desarrollo ha sido encomendado al estudio Ninja Theory en colaboración con Capcom US. En el juego veremos a un Dante más rápido y demoledor que nunca, pero también mucho más joven e inexperto que, aun así, no pierde sus incipientes aires de chulería.</li>
+<li>Plataforma: PlayStation 3 - PC - X360</li>
+<li>Desarrollador: Ninja Theory</li>
+<li>Distribuidor: Capcom</li>
+<li>Jugadores: 1</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 15 de enero de 2013 (Pegi: +16)</li>', 259, 1, 'img/accion/dmc.jpg'),
+('Watch Dogs','<li>Watch Dogs está ambientado en un futuro cercano en el que el jugador tiene en su poder infinidad de gadgets y de artilugios tecnológicos con los que sembrar el caos a su alrededor para llevar a cabo diferentes tipos de misiones. Un planteamiento mitad aventura mitad shooter que ha levantado admiración durante su presentación en sociedad.</li>
+<li>Plataforma: PlayStation 3 - PC - X360 - PS4 - Wii U</li>
+<li>Desarrollador: UbiSoft</li>
+<li>Distribuidor: UbiSoft</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 2013 (Por determinar) (Pegi: +18)</li>', 199, 1, 'img/accion/watchdogs.jpg'),
 ('The Cave','<li>Juego sin reseña</li>', 99, 2, NULL),
 ('Amnesia: A Machine for Pigs','<li>Juego sin reseña</li>', 300, 2, NULL),
 ('Lucius','<li>Juego sin reseña</li>', 259, 2, NULL),
 ('Asylum','<li>Juego sin reseña</li>', 156, 2, NULL),
 ('Anna','<li>Juego sin reseña</li>', 109, 2, NULL),
-('Los Sims 3','<li>Juego sin reseña</li>', 0, 3, NULL),
-('Plants Vs. Zombies','<li>Juego sin reseña</li>', 0, 3, NULL),
-('Spore','<li>Juego sin reseña</li>', 59, 3, NULL),
-('Braid','<li>Juego sin reseña</li>', 149, 3, NULL),
-('Word of Goo','<li>Juego sin reseña</li>', 149, 3, NULL),
-('Need For Speed Most Wanted','<li>Juego sin reseña</li>', 299, 4, NULL),
-('GRID 2','<li>Juego sin reseña</li>', 399, 4, NULL),
+('Los Sims 3','<li>Tercera parte del que probablemente sea el simulador de vida virtual más famoso del mundo. Nuevas características jugables, un nuevo Creador de Sims, personalidades más realistas y un poderoso sistema de personalización, son algunas de sus novedades.</li>
+<li>Plataforma: PlayStation 3 - PC - X360 - Wii - 3DS - DS - iPhone - Mac</li>
+<li>Desarrollador: The Sims Studio</li>
+<li>Distribuidor: EA</li>
+<li>Jugadores: 1</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 26 de octubre de 2010 (Pegi: +12)</li>', 0, 3, NULL),
+('Plants Vs. Zombies','<li>Una inmensa horda de divertidos zombis se dispone a invadir tu casa, y tu única línea de defensa es un arsenal de 49 plantas "atrapazombis". Este es el divertido plateamiento de este interesante juego mezcla de estrategia, acción y puzle.</li>
+<li>Plataforma: PlayStation 3 - PC - X360 - 3DS - DS - Vita - Web - iPhone - iPad - Android - Mac</li>
+<li>Desarrollador: PopCap</li>
+<li>Distribuidor: PopCap</li>
+<li>Jugadores: 1-2 (Cooperativo: 2)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 9 de febrero de 2011 (Pegi: +12)</li>', 0, 3, NULL),
+('Spore','<li>Will Wright creador del exitoso "Los Sims" es el responsable de "Spore", un innovador juego evolutivo en el que el jugador creará sus propias criaturas en un mundo que se desarrolla por toda la galaxia.</li>
+<li>Plataforma: PC - DS - iPhone - Mac</li>
+<li>Desarrollador: Maxis</li>
+<li>Distribuidor: EA</li>
+<li>Jugadores: 1</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 5 de septiembre de 2008 (Pegi: +12)</li>', 59, 3, NULL),
+('Braid','<li>Desarrollado por el diseñador experimental Jonathan Blow, ganador del "Independent Games Festival", Braid es un juego que mezcla plataformas 2D con elementos puzle dentro de un sistema de juego que permite manipular el tiempo de la acción.</li>
+<li>Plataforma: PlayStation 3 - PC - X360</li>
+<li>Desarrollador: Hothead Games</li>
+<li>Distribuidor: Sony</li>
+<li>Jugadores: 1</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 17 de diciembre de 2009 (Pegi: +7)</li>', 149, 3, NULL),
+('Word of Goo','<li>Es un original juego, tanto en concepto como en estilo gráfico, en el que es posible construir a base de una gotas llamadas "goo", todo tipo de impresionantes estructuras con las que resolver montones de ingeniosos puzles.</li>
+<li>Plataforma: PC - Wii - iPhone - iPad - Android - Mac</li>
+<li>Desarrollador: 2D Boy</li>
+<li>Distribuidor: 2D Boy</li>
+<li>Jugadores: 1-2</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 19 de junio de 2009 (Pegi: +3)</li>', 149, 3, NULL),
+('Need For Speed: Most Wanted','<li>Juego sin reseña</li>', 299, 4, NULL),
+('GRID 2','<li>Secuela del trepidante juego de carreras desarrollado por Codemasters, que cuenta entre otras novedades con pleno la última evolución del premiado motor gráfico EGO.</li>
+<li>Plataforma: PlayStation 3 - PC - X360</li>
+<li>Desarrollador: Codemasters</li>
+<li>Distribuidor: Codemasters</li>
+<li>Lanzamiento: 31 de mayo de 2013</li>', 399, 4, NULL),
 ('Euro Truck','<li>Juego sin reseña</li>', 99, 4, NULL),
 ('WRC 3','<li>Juego sin reseña</li>', 109, 4, NULL),
 ('ToCA Race Driver 3','<li>Juego sin reseña</li>', 0, 4, NULL),
