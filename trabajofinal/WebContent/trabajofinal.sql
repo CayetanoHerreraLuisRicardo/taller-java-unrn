5f812c8d989020506c290eb894d935cf70ad3e72
Winderfind@gmail.com <Winderfind@gmail.com@33d2613c-2304-736f-4ed5-b9ea7df18124>
2013-04-26 13:40:44 +0000

    Edición de imágenes.

--- trabajofinal/WebContent/trabajofinal.sql
+++ trabajofinal/WebContent/trabajofinal.sql
@@ -270,7 +270,7 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Textos: Español</li>
 	<li>Voces: Español</li></ul>
 </li>
-<li>Lanzamiento: 24 de febrero de 2006 (Pegi: +12)</li>', 0, 4, 'img/tocarace3.jpg'),
+<li>Lanzamiento: 24 de febrero de 2006 (Pegi: +12)</li>', 145, 4, 'img/tocarace3.jpg'),
 ('F1 2012','<li>Videojuego con licencia oficial de la temporada 2012 del campeonato de Fórmula Uno que que incluye todos los equipos, pilotos y circuitos oficiales. Esta entrega incluye varias novedades y mejoras respecto a F1 2011, entre ellas la incorporación de la "Prueba para Jóvenes Pilotos".</li>
 <li>Plataforma: PlayStation 3 - PC - X360 - Mac</li>
 <li>Desarrollador: Codemasters</li>
@@ -293,21 +293,125 @@ INSERT INTO `trabajofinal`.`producto` (`nombre`,`descrip`,`precio`,`categoria_id
 	<li>Voces: Español</li></ul>
 </li>
 <li>Lanzamiento: 14 de septiembre de 2011 (Pegi: +3)</li>', 79, 4, 'img/trackmania2.jpg'),
-('FIFA 13','<li>Juego sin reseña</li>', 389, 5, NULL),
-('PES 2013','<li>Juego sin reseña</li>', 389, 5, NULL),
-('NBA 2K13','<li>Juego sin reseña</li>', 299, 5, NULL),
-('FIFA Manager 13','<li>Juego sin reseña</li>', 219, 5, NULL),
+('FIFA 13','<li>Edición de la temporada 2012-2013 de la popular saga de videojuegos de fútbol de EA Sport.</li>
+<li>Plataforma: PlayStation 3 - PC - X360 - Wii - 3DS - PSP - Vita - PS2 - iPhone - iPad - Android - Wii U</li>
+<li>Desarrollador: EA Games</li>
+<li>Distribuidor: EA Sports</li>
+<li>Jugadores: 1-22 (Competitivo: 2-22 / Cooperativo: 2-11)</li>
+<li>Soporta: PlayStation Move</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 27 de septiembre de 2012 (Pegi: +3)</li>', 389, 5, 'img/fifa13.jpg'),
+('PES 2013','<li>Nueva entrega de la popular serie de videojuegos de fútbol de Konami correspondiente a la temporada 2012-2013.</li>
+<li>Plataforma: PlayStation 3 - PC - X360 - Wii - 3DS - PSP - PS2 - iPhone - iPad - Android</li>
+<li>Desarrollador: Konami</li>
+<li>Distribuidor: Konami</li>
+<li>Jugadores: 1-8 (Competitivo: 2-8 / Cooperativo: 2-8)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 20 de septiembre de 2012 (Pegi: +3)</li>', 389, 5, 'img/pes2013.jpg'),
+('NBA 2K13','<li>Nueva entrega de la consagrada saga de baloncesto NBA 2K. Una de sus grandes novedades es la incorporación de comentarios en español.</li>
+<li>Plataforma: PlayStation 3 - PC - X360 - Wii - PSP - iPhone - iPad - Android - Wii U</li>
+<li>Desarrollador: Visual Concepts</li>
+<li>Distribuidor: 2K Sports</li>
+<li>Jugadores: 1-4 (Competitivo: 2-4)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 5 de octubre de 2012 (Pegi: +3)</li>', 299, 5, 'img/nba2k13.jpg'),
+('FIFA Manager 13','<li>Nueva edición dentro de la serie FIFA Manager, destinada a ponernos al cargo de un equipo de fútbol como gestores y entrenadores, desde labores relacionadas con las instalaciones hasta la propia dirección de los futbolistas. Esta edición estrena un conjunto de novedades como el Team Dynamics, nuevo interfaz y herramientas de gestión.</li>
+<li>Plataforma: PC</li>
+<li>Desarrollador: Bright Future Gmbh</li>
+<li>Distribuidor: EA Sports</li>
+<li>Lanzamiento: 25 de octubre de 2012 (Pegi: +3)</li>', 219, 5, 'img/fifamanager13.jpg'),
 ('The Hunter','<li>Juego sin reseña</li>', 59, 5, NULL),
-('Total War: Rome II','<li>Juego sin reseña</li>', 459, 6, NULL),
-('SimCity','<li>Juego sin reseña</li>', 0, 6, NULL),
-('Impire','<li>Juego sin reseña</li>', 99, 6, NULL),
-('Age Of Empire II HD','<li>Juego sin reseña</li>', 0, 6, NULL),
-('StarCraft 2','<li>Juego sin reseña</li>', 199, 6, NULL),
-('Neverwinter','<li>Juego sin reseña</li>', 219, 7, NULL),
-('TERA: True Action Combat','<li>Juego sin reseña</li>', 189, 7, NULL),
-('The War Z','<li>Juego sin reseña</li>', 179, 7, NULL),
-('Dota 2','<li>Juego sin reseña</li>', 180, 7, NULL),
-('League of Legends','<li>Juego sin reseña</li>', 0, 7, NULL),
+('Total War: Rome II','<li>La saga Total War estrena segunda entrega de Rome abarcando uno de los periodos de la historia más célebres. El videojuego combina la mayor campaña basada en turnos que se haya realizado por Creative Assembly, junto con las mayores batallas en tiempo real.</li>
+<li>Plataforma: PC</li>
+<li>Desarrollador: Creative Assembly</li>
+<li>Distribuidor: SEGA</li>
+<li>Lanzamiento: 2013 (Por determinar)</li>', 459, 6, 'img/tw_rome2.jpg'),
+('SimCity V','<li>Quinta entrega de la afamada serie de juegos de gestión y estrategia de Maxis y Electronic Arts. Un buen lavado de cara para la franquicia que ofrece un mayor componente social y un acercamiento igual de apasionante para hardcores de la IP y para neófitos en el concepto de la "estrategia civil".</li>
+<li>Plataforma: PC - Mac</li>
+<li>Desarrollador: Maxis</li>
+<li>Distribuidor: EA</li>
+<li>Jugadores: 1-16 (Cooperativo: 2-16)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 7 de marzo de 2013 (Pegi: +7)</li>', 0, 6, 'img/simcity5.jpg'),
+('Impire','<li>Juego de acción, estrategia y rol desarrollado por Cyanide que nos pondrá en la piel de un señor de las mazmorras, el demonio Bjal-Abaddon, que deberá defenderse de las incursiones de héroes y nobles caballeros haciendo uso de trampas y un montón de criaturas monstruosas a las que se podrá personalizar con todo tipo de armas y conjuntos de guerra.</li>
+<li>Plataforma: PC</li>
+<li>Desarrollador: Cyanide</li>
+<li>Distribuidor: Paradox Interactive</li>
+<li>Jugadores: 1-2 (Competitivo: 2-4 / Cooperativo: 2)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 14 de febrero de 2013 (Pegi: +16)</li>', 99, 6, 'img/impire.jpg'),
+('Age Of Empire II HD','<li>El clásico Age of Empires II regresa al mercado con esta remasterización a la alta definición que incluye gráficos adaptados a las 1080p, texturas más definidas, así como mejores efectos de iluminación. El juego trae consigo además la expansión Conquerors.</li>
+<li>Plataforma: PC</li>
+<li>Desarrollador: Hidden Path</li>
+<li>Distribuidor: Microsoft</li>
+<li>Lanzamiento: 9 de abril de 2013 (Pegi: +12)</li>', 0, 6, 'img/aoe2.jpg'),
+('StarCraft 2','<li>Continuación del popular juego de estrategia creado por Blizzard. Protoss, Terran y Zerg, vuelven a combatir en tiempo real, pero esta vez en 3D y con nuevas habilidades e incorporaciones. Esta segunda entrega de Starcraft se divide en tres capítulos: El primero, StarCraft II Terrans: Wings of Liberty. El segundo StarCraft II Zerg: Heart of the Swarm. Y el tercero y final, StarCraft II Protoss: Legacy of the Void.</li>
+<li>Plataforma: PC - Mac</li>
+<li>Desarrollador: Blizzard</li>
+<li>Distribuidor: Activision Blizzard</li>
+<li>Jugadores: 1-8 (Competitivo: 2-8 / Cooperativo: 2)</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 27 de julio de 2010 (Pegi: +16)</li>', 199, 6, 'img/starcraft2.jpg'),
+('Neverwinter','<li>Cryptic Studios creadores de títulos como City of Heroes, Champions Online o Star Trek Online, firman este videojuego basado en el universo de Dungeons & Dragons, y más concretamente en la ciudad de Neverwinter como su propio nombre indica.</li>
+<li>Plataforma: PC</li>
+<li>Desarrollador: Cryptic Studios</li>
+<li>Distribuidor: Atari</li>
+<li>Lanzamiento: 1º trimestre de 2013</li>', 219, 7, 'img/neverwinter.jpg'),
+('TERA: True Action Combat','<li>MMORPG desarrollado por Bluehole Studio, con un potente apartado gráfico y un innovador sistema de combate, en el que podremos atacar a los enemigos sin necesidad de seleccionarlos.</li>
+<li>Plataforma: PC</li>
+<li>Desarrollador: Bluehole Studio</li>
+<li>Distribuidor: Frogster Interactive</li>
+<li>Jugadores: Masivo online</li>
+<li>Idioma:<ul>
+	<li>Manual: Inglés</li>
+	<li>Textos: Inglés</li>
+	<li>Voces: Inglés</li></ul>
+</li>
+<li>Lanzamiento: 3 de mayo de 2012 (Pegi: +12)</li>', 189, 7, 'img/tera.jpg'),
+('The War Z','<li>The War Z es un simulador de supervivencia, multijugador masivo online, ambientada en un mundo abierto infestado de zombis.</li>
+<li>Plataforma: PC
+<li>Desarrollador: Hammerpoint Interactive</li>
+<li>Distribuidor: Arktos Entertainment</li>
+<li>Lanzamiento: 17 de diciembre de 2012 (Pegi: +18)</li>', 179, 7, 'img/thewarz.jpg'),
+('Dota 2','<li>Segunda entrega de Defense of the Ancients, cuyo desarrollo surge de la colaboración de Valve con Ice Frog. Y la cual respetará los más de 100 héroes del original, junto con sus items y las distintas mejoras, dejando de lado el motor de Warcraft III y se apuesta por el Source de la propia Valve.</li>
+<li>Plataforma: PC - Mac</li>
+<li>Desarrollador: IceFrog</li>
+<li>Distribuidor: Valve</li>
+<li>Lanzamiento: 2013 (Por determinar)</li>', 180, 7, 'img/dota2.jpg'),
+('League of Legends','<li>Juego de acción competitiva con dosis de RPG y estrategia, en un rico universo MMO (multijugador masivo online). La historia se recrea en un mágico mundo asolado por el conflicto de Rune Wars.</li>
+<li>Plataforma: PC</li>
+<li>Desarrollador: Riot Games</li>
+<li>Distribuidor: Riot Games</li>
+<li>Idioma:<ul>
+	<li>Manual: Español</li>
+	<li>Textos: Español</li>
+	<li>Voces: Español</li></ul>
+</li>
+<li>Lanzamiento: 30 de octubre de 2009 (Pegi: +12)</li>', 0, 7, 'img/lol.jpg'),
 ('Skyrim-Dragonborn','<li>Juego sin reseña</li>', 189, 8, NULL),
 ('Diablo III','<li>Juego sin reseña</li>', 209, 8, NULL),
 ('LOTR: La Guerra del Norte','<li>Juego sin reseña</li>', 199, 8, NULL),
